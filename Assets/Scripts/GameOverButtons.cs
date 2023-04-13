319a84a972a03c897680a2f329859a1d2a31127d
Kostas Liutkus <kostliutk2@gmail.com>
2023-04-13 20:50:19 +0300

    All UI buttons implementation
    to implement the scene should start with main menu instead of the game

---
+++ Assets/Scripts/GameOverButtons.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button restartButton;
+    public Button mainMenuButton;
+    public GameObject MainMenuUI;
+    public GameObject gameOverUI;
+    public GameObject Shop;
+    void Start()
+    {
+        Button restart = restartButton.GetComponent<Button>();
+        Button mainMenu = mainMenuButton.GetComponent<Button>();
+        
+        restart.onClick.AddListener(RestartGame);
+        mainMenu.onClick.AddListener(OpenMainMenu);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenMainMenu()
+    {
+        Shop.SetActive(false);
+        gameOverUI.SetActive(false);
+        MainMenuUI.SetActive(true);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("RESTART BUTTON WORKING");
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,16 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
+	public Button startButton;
+	public Button quitButton;
+	public GameObject Shop;
     public void Start()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		Button quit = quitButton.GetComponent<Button>();
+		Button Start = startButton.GetComponent<Button>();
+        Start.onClick.AddListener(StartGame);
+
+        quit.onClick.AddListener(QuitGame);
     }
-    public void Quit() {
-        {
-            Application.Quit();
-        }
+    public void QuitGame() {
+        Application.Quit();
+		Debug.Log("Game Quit");
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		Shop.SetActive(true);
+        Debug.Log("Start BUTTON WORKING");
     }
 }
