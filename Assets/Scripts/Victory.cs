b51719bce7c397a67a6d235bc4db7ce092c1d531
Clyde <61326797+Sadialk@users.noreply.github.com>
2023-05-08 22:30:01 +0300

    kazkas

--- Assets/Scripts/Victory.cs
+++ Assets/Scripts/Victory.cs
@@ -3,21 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine;
 
 public class Victory : MonoBehaviour
 {
-    public Button ContinueB;
-    public GameObject VictoryUI;
     public GameObject MainMenuUI;
-    void Start()
-    {
-        Button Continue = ContinueB.GetComponent<Button>();
-        
-        Continue.onClick.AddListener(OpenMainMenu);
-    }
     public void OpenMainMenu()
     {
-        VictoryUI.SetActive(false);
-        MainMenuUI.SetActive(true);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
